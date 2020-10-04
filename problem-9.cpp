b29c8475942db58210b22e8f99740b5c5ceaf007
AbhishekSharma20 <asharma20011994@gmail.com>
2020-10-04 14:23:15 +0530

    Create problem-9.cpp

---
+++ problem-9.cpp
@@ -0,0 +1,16 @@
+/*
+* Using pythogarous triplets rule,
+* for m>n (this part is very important for later stage)
+* a = m^2 -n^2
+* b = 2.m.n
+* c = m^2 + n^2
+* (a,b,c) form pythogarous triplets, i.e => a^2 + b^2 = c^2
+* also, a + b + c = 1000 which further reduces the problems to
+* m*(m+n) = 500.
+* Hence problem reduces to "Find the two factors of 500 which statisfy this relationship. remember  m> n ".
+* Answer: (m,n) =(20,5) 
+* => (a,b,c) = (375,200,425)
+* => a*b*c  = 375*200*425 = 31875000
+*/
+
+
